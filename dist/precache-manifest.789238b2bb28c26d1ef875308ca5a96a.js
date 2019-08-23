d13f20bc1d387a66f0a7c395ca1405c5adfa8837
edipkavvas <edipkavvas@hotmail.com>
2019-08-23 17:03:34 +0300

    server app

---
+++ dist/precache-manifest.789238b2bb28c26d1ef875308ca5a96a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2786f46a8fb2afb56aa7f5dac0afad58",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3179617b41bfcc82647a",
+    "url": "/static/css/main.b2a887c6.chunk.css"
+  },
+  {
+    "revision": "b982fd83cd18f8c99bfa",
+    "url": "/static/js/2.cd57c468.chunk.js"
+  },
+  {
+    "revision": "3179617b41bfcc82647a",
+    "url": "/static/js/main.2aa82225.chunk.js"
+  },
+  {
+    "revision": "85304a0d8147cfb7967e",
+    "url": "/static/js/runtime~main.077bb605.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
